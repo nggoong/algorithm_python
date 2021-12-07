d91c80268f5b1feb53268a08477e3cd6a2d9544e
nggoong <nggoong@naver.com>
2021-12-08 00:28:25 +0900

    sorted()의 위치와 원소를 완전 삭제하는 방법으로 변경

--- BAEKJOON/sevenDwarfs/sevenDwarfs.py
+++ BAEKJOON/sevenDwarfs/sevenDwarfs.py
@@ -1,17 +1,19 @@
 heights = [int(input()) for i in range(9)]
 heightsSum = sum(heights)
 
-copyArr = sorted(heights)
 
 for i in range(9):
+    copyArr = sorted(heights)
     for j in range(i + 1, 9):
         if heightsSum - (heights[i] + heights[j]) == 100:
+            copyArr.remove(heights[i])
+            copyArr.remove(heights[j])
 
-            for k in range(9):
-                if copyArr[k] == heights[i] or copyArr[k] == heights[j]:
-                    continue
-                else:
-                    print(copyArr[k])
+            for k in copyArr:
+                print(k)
+
+            if len(copyArr) < 9:
+                break
 
 
     
